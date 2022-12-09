af76e29c47c6646e888c7af33bb11fa3d85d09d3
tanymanera <gaetanomanera@outlook.it>
2022-12-09 20:11:13 +0100

    Make a footer

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from fpdf import FPDF
 import pandas as pd
+from footer import footer
 
 df = pd.read_csv('topics.csv')
 
 pdf = FPDF(orientation='P', unit='mm', format='A4')
+pdf.set_auto_page_break(auto=False, margin=0)
+
 
 for index, row in df.iterrows():
     pdf.add_page()
     pdf.set_font(family='Times', style='B', size=24)	# size è in pt e non in mm (?)
     pdf.cell(w=0, h=12, txt=row['Topic'], align="L", ln=1, border=0)
     pdf.line(x1=10, y1=22, x2=200, y2=22)   # A4 is 210 mm width
+    footer(pdf, row['Topic'])
 
     for page in range(row['Pages'] - 1):
         pdf.add_page()
+        pdf.set_font(family='Times', style='B', size=24)	# size è in pt e non in mm (?)
+        pdf.cell(w=0, h=12, align="L", ln=1, border=0)
+        footer(pdf, row['Topic'])
 
 pdf.output('output.pdf')
